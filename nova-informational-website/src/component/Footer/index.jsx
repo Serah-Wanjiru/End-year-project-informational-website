b87db7c1bd5fa32936820c52ca6160fc4437507e
Serah-Wanjiru <wanjirumburuuu@gmail.com>
2023-09-05 16:01:13 -0400

    Adding component folder and images

---
+++ nova-informational-website/src/component/Footer/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+    <footer>
+        
+            
+
+            <div id="footer">
+                <div className="containers text-center">
+                    <p className="try">
+                        &copy; 2023 Copyright @Nova Truxapp website{" "}
+                        <a href="/" rel="nofollow">
+                            Nova
+                        </a>
+                    </p>
+                </div>
+            </div>
+           
+
+        
+        </footer>
+    )
+}
+export default Footer;
---
+++ nova-informational-website/src/component/layout/layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "../navigation/navigation";
+import Footer from "../Footer";
+
+
+
+
+
+const Layout =()=>{
+    return(
+        <div>
+            <Navigation/>
+            <Footer/>
+            
+            
+        </div>
+    )
+}
+export default Layout;
