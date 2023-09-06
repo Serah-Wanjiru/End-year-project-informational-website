9403e0a01ba1fe8f80b7e121b9957f60f513cf1e
Bridet-Mutesi <bridgettemutesi@gmail.com>
2023-09-06 21:47:18 +0300

    Added media Queries and changed some code

--- nova-informational-website/src/component/Team/indext.test.js
+++ nova-informational-website/src/component/Team/indext.test.js
@@ -1,31 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import Team from "./";
+import { render, screen } from "@testing-library/react";
+import Team from ".";
 
-describe("Team component", () => {
-  it("renders team members correctly", () => {
-    const { getAllByText, getByAltText } = render(<Team />);
-
-    expect(getAllByText("Software Developer")).toHaveLength(3);
-
-    expect(getAllByText("Bridget Mutesi")).toHaveLength(1);
-    expect(getAllByText("Software Developer || UI/UX Designer")).toHaveLength(1);
-    expect(getByAltText("Bridget Mutesi")).toBeInTheDocument();
-
-    expect(getAllByText("Serah Wanjiru")).toHaveLength(1);
-    expect(getAllByText("Software Developer")).toHaveLength(3); 
-    expect(getByAltText("Sera")).toBeInTheDocument();
-
-    expect(getAllByText("Faith Mwamkinga")).toHaveLength(1);
-    expect(getAllByText("Software Developer || Product Manager")).toHaveLength(1);
-    expect(getByAltText("Mwamkinga")).toBeInTheDocument();
-
-    expect(getAllByText("Maureen Ougo")).toHaveLength(1);
-    expect(getAllByText("Software Developer")).toHaveLength(3); 
-    expect(getByAltText("Maureen")).toBeInTheDocument();
-
-    expect(getAllByText("Rita Khaseyi")).toHaveLength(1);
-    expect(getAllByText("Software Developer")).toHaveLength(3); 
-    expect(getByAltText("Rita")).toBeInTheDocument();
+describe("Team Component", () => {
+  test("renders the Meet the Team heading", () => {
+    render(<Team />);
+    const headingElement = screen.getByText("Meet the Team");
+    expect(headingElement).toBeInTheDocument();
   });
+
+  test("renders the team introduction text", () => {
+    render(<Team />);
+    const introTextElement = screen.getByText(
+      "We are a team of great engineers aimed to provide great services and experience with the app"
+    );
+    expect(introTextElement).toBeInTheDocument();
+  });
+
+ 
 });
