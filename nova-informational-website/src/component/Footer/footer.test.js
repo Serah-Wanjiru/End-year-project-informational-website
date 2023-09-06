c0b46494c1bb837614fb05d596b343cd8a0a40b6
Serah-Wanjiru <wanjirumburuuu@gmail.com>
2023-09-05 22:36:27 -0400

    Added footer test file and navigation test files

--- nova-informational-website/src/component/Footer/footer.test.js
+++ nova-informational-website/src/component/Footer/footer.test.js
@@ -6,16 +6,16 @@ describe("Footer Component", () => {
   test("renders the correct copyright text", () => {
     render(<Footer />);
     
-    // Check if the copyright text is rendered
+   
     const copyrightText = screen.getByText(/copyright @nova truxapp/i);
     expect(copyrightText).toBeInTheDocument();
 
-    // Check if the link is rendered within the text
+   
     const link = screen.getByRole("link", { name: /nova/i });
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute("href", "/");
 
-    // Check if the text content within the link is correct
+   
     expect(link.textContent).toBe("Nova");
   });
 
--- nova-informational-website/src/component/navigation/navigation.test.js
+++ nova-informational-website/src/component/navigation/navigation.test.js
@@ -33,27 +33,27 @@ describe("Navigation Component", () => {
     test("renders correct names for menu links", () => {
       render(<Navigation />);
       
-      // Check if the home link has the correct name
+     
       const homeLink = screen.getByRole("link", { name: /home/i });
       expect(homeLink).toHaveTextContent("Home");
   
-      // Check if the about link has the correct name
+    
       const aboutLink = screen.getByRole("link", { name: /about/i });
       expect(aboutLink).toHaveTextContent("About");
   
-      // Check if the values link has the correct name
+   
       const valuesLink = screen.getByRole("link", { name: /values/i });
       expect(valuesLink).toHaveTextContent("Values");
   
-      // Check if the products link has the correct name
+    
       const productsLink = screen.getByRole("link", { name: /products/i });
       expect(productsLink).toHaveTextContent("Products");
   
-      // Check if the team link has the correct name
+   
       const teamLink = screen.getByRole("link", { name: /team/i });
       expect(teamLink).toHaveTextContent("Team");
   
-      // Check if the contact link has the correct name
+     
       const contactLink = screen.getByRole("link", { name: /contact/i });
       expect(contactLink).toHaveTextContent("Contact");
     });
