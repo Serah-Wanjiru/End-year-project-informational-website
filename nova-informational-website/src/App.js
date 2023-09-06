3042653e7771796eca1c2e0899a51f12dd1cc836
rita-khaseyi <ritakhaseyi@gmail.com>
2023-09-06 21:00:39 +0300

    added Landing page section

---
+++ nova-informational-website/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Contact from './component/Contacts';
+
+function App() {
+  return (
+    <div className="App">
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
