4bb1ec9ca8ab900034ab48df4a4fbcfdf4e96c19
Faith Mwamkinga <faithmwamkinga@gmail.com>
2023-09-06 09:32:57 +0300

    added unit tests for about section

--- nova-informational-website/src/component/about/about.jsx
+++ nova-informational-website/src/component/about/about.jsx
@@ -19,19 +19,23 @@ const About = () => {
                 Who <span>we Are</span>{" "}
               </h2>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Minima, magni in fugit possimus dicta voluptatum a nihil natus
-                repudiandae id harum mollitia amet quae, aliquam obcaecati
-                dolorem iure numquam eaque.
+              Welcome to our innovative Trux App solution designed
+              to alleviate congestion in the Busia-Malaba area. We are 
+              committed to revolutionalizing transportation by
+              providing efficient and streamlined logistics for truck 
+              drivers and business. Join us in shaping a smoother,
+              more sustainable future for the region’s transportation
+              landscape.
               </p>
               <h3>
                 Our <span>Goal</span>
               </h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Delectus ullam suscipit quo facere eius, possimus provident
-                commodi quod incidunt corporis laborum facilis iusto nesciunt
-                fugiat eligendi maiores sapiente. Nemo, deleniti?
+              Welcome to our innovative Trux App solution designed
+              to alleviate congestion in the Busia-Malaba area. We are 
+              committed to revolutionalizing transportation by
+              providing efficint and stremlined logisics for truck 
+              drivers and business. 
               </p>
             </div>
           </div>
@@ -49,10 +53,13 @@ const About = () => {
                 Our <span>Vision</span>
               </h2>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
-                tenetur officia nesciunt, itaque quam obcaecati suscipit sit
-                mollitia ex possimus facere reiciendis amet, expedita asperiores
-                quo explicabo nihil modi sapiente!
+              Our vision is to create a seamless and congestion-free
+              transportation environment in the Buia-Malaba area. We envision
+              a future where our Trux App serves as the catalyst for efficient
+              logistics, intelligent route planning, and harmonized technology
+              and innovation, we aim to redefine he way goods are transported,
+              leading to enhanced productivity, reduced environment impact, 
+              and a thriving regional economy.
               </p>
             </div>
           </div>
@@ -84,10 +91,14 @@ const About = () => {
                 Our <span>Mission</span>{" "}
               </h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-                odio aliquam quae iste, eaque molestiae iure tenetur quisquam
-                porro fugiat delectus saepe. Cupiditate voluptas earum delectus
-                incidunt voluptatum eligendi voluptatibus?
+              Our mission is to revolutionize truck logistics in the Busia -
+              Malaba area through our advanced TruxApp. We are dedicated to
+              providing truck drivers and businesses with a comprehensive 
+              platform that offers optimized route planning, real-time updates
+              and efficient coordination. By streamlining operations and reducing
+              congestion, we aim to enhance overall productivity, promote sustainable 
+              practises and contribute to the development and prosperity of the region.
+
               </p>
             </div>
           </div>
