9d65e131d36add96dbe8b53630557d04da20f1b5
Student <ougomaureenakinyi@gmail.com>
2023-09-06 21:22:32 +0300

    Updated changes from reviews

--- nova-informational-website/src/App.js
+++ nova-informational-website/src/App.js
@@ -1,15 +1,12 @@
 
 import './App.css';
-import Values from './component/Values';
-import Products from './component/Products';
 
 
 function App() {
   return (
   
    <div>
-    <Values />
-    <Products />
+    
    </div>
   );
 }
